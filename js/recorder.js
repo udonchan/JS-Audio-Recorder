ea61a2f325cd8facd93ad08bfd290f6211e7b126
鈴木 Udonchan 督史 <suzuki.masafumi@lab.ntt.co.jp>
2012-10-11 10:51:55 +0900

    Instance method defined in Recorder.prototype.
    I was referring to yudai's repository(But his code does not work).

--- js/recorder.js
+++ js/recorder.js
@@ -1,82 +1,86 @@
 (function(window){
 
-  var WORKER_PATH = 'js/recorderWorker.js';
+    var WORKER_PATH = 'js/recorderWorker.js';
 
-  var Recorder = function(source, cfg){
-    var config = cfg || {};
-    var bufferLen = config.bufferLen || 4096;
-    this.context = source.context;
-    this.node = this.context.createJavaScriptNode(bufferLen, 2, 2);
-    var worker = new Worker(config.workerPath || WORKER_PATH);
-    worker.postMessage({
-      command: 'init',
-      config: {
-        sampleRate: this.context.sampleRate
-      }
-    });
-    var recording = false,
-      currCallback;
+    var config;
+    var worker;
+    var currCallback;
+    var recording = false;
+    
+    var Recorder = function(source, cfg){
+	config = cfg || {};
+	var bufferLen = config.bufferLen || 4096;
+	this.context = source.context;
+	this.node = this.context.createJavaScriptNode(bufferLen, 2, 2);
 
-    this.node.onaudioprocess = function(e){
-      if (!recording) return;
-      worker.postMessage({
-        command: 'record',
-        buffer: [
-          e.inputBuffer.getChannelData(0),
-          e.inputBuffer.getChannelData(1)
-        ]
-      });
+	this.node.onaudioprocess = function(e){
+	    if (!recording) return;
+	    worker.postMessage({
+		command: 'record',
+		buffer: [
+		    e.inputBuffer.getChannelData(0),
+		    e.inputBuffer.getChannelData(1)
+		]
+	    });
+	};
+
+	worker = new Worker(config.workerPath || WORKER_PATH);
+	worker.postMessage({
+	    command: 'init',
+	    config: {
+		sampleRate: this.context.sampleRate
+	    }
+	});
+
+	worker.onmessage = function(e){
+	    var blob = e.data;
+	    currCallback(blob);
+	};
+	
+	source.connect(this.node);
+	this.node.connect(this.context.destination);    //this should not be necessary
+    };
+
+    Recorder.prototype.configure = function(cfg){
+	for (var prop in cfg){
+            if (cfg.hasOwnProperty(prop)){
+		config[prop] = cfg[prop];
+            }
+	}
+    };
+
+    Recorder.prototype.record = function(){
+    	recording = true;
+    };
+
+    Recorder.prototype.stop = function(){
+	recording = false;
+    };
+
+    Recorder.prototype.clear = function(){
+	worker.postMessage({ command: 'clear' });
+    };
+
+    Recorder.prototype.exportWAV = function(cb, type){
+	currCallback = cb || config.callback;
+	type = type || config.type || 'audio/wav';
+	if (!currCallback) throw new Error('Callback not set');
+	worker.postMessage({
+            command: 'exportWAV',
+            type: type
+	});
+    };
+
+    Recorder.forceDownload = function(blob, filename){
+	var url = (window.URL || window.webkitURL).createObjectURL(blob);
+	var link = window.document.createElement('a');
+	link.href = url;
+	link.download = filename || 'output.wav';
+	var click = document.createEvent("Event");
+	click.initEvent("click", true, true);
+	link.dispatchEvent(click);
     }
 
-    this.configure = function(cfg){
-      for (var prop in cfg){
-        if (cfg.hasOwnProperty(prop)){
-          config[prop] = cfg[prop];
-        }
-      }
-    }
-
-    this.record = function(){
-      recording = true;
-    }
-
-    this.stop = function(){
-      recording = false;
-    }
-
-    this.clear = function(){
-      worker.postMessage({ command: 'clear' });
-    }
-
-    this.exportWAV = function(cb, type){
-      currCallback = cb || config.callback;
-      type = type || config.type || 'audio/wav';
-      if (!currCallback) throw new Error('Callback not set');
-      worker.postMessage({
-        command: 'exportWAV',
-        type: type
-      });
-    }
-
-    worker.onmessage = function(e){
-      var blob = e.data;
-      currCallback(blob);
-    }
-
-    source.connect(this.node);
-    this.node.connect(this.context.destination);    //this should not be necessary
-  };
-
-  Recorder.forceDownload = function(blob, filename){
-    var url = (window.URL || window.webkitURL).createObjectURL(blob);
-    var link = window.document.createElement('a');
-    link.href = url;
-    link.download = filename || 'output.wav';
-    var click = document.createEvent("Event");
-    click.initEvent("click", true, true);
-    link.dispatchEvent(click);
-  }
-
-  window.Recorder = Recorder;
+    window.Recorder = Recorder;
 
 })(window);
